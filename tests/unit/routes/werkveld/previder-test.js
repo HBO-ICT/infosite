80fb3c4d5825acf8d24e1eeb9dce11061936a222
justniek <31650164+justniek@users.noreply.github.com>
2017-10-17 12:21:36 +0200

    previder en switch

--- tests/unit/routes/werkveld/previder-test.js
+++ tests/unit/routes/werkveld/previder-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:werkveld/switch', 'Unit | Route | werkveld/switch', {
+moduleFor('route:werkveld/previder', 'Unit | Route | werkveld/previder', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
